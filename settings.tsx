b75b16fe740e34a229a53db3663ec28eebddc16b
mbkilci <72472303+mbkilci@users.noreply.github.com>
2024-05-07 03:33:20 +0300

    Update settings.tsx

--- settings.tsx
+++ settings.tsx
@@ -1,9 +1,9 @@
 // Purpose: Contains texts that will be used in the UI when rendering the buttons
 // label has to be the exact same as in your candy machine config
 export const mintText = [
-  { label: "WL", mintText: "WL mint paying with 0.2 SOL", buttonLabel: "Mint now!", header: "WL Mint" },
-  { label: "OG", mintText: "OG Mint Paying with 0.3 SOL", buttonLabel: "Mint now!", header: "OG Mint" },
-  { label: "Public", mintText: "Mint Paying with 0.4 SOL", buttonLabel: "Mint now!", header: "Public Mint" },
+  { label: "WL", mintText: "0.2 SOL", buttonLabel: "Mint now!", header: "WL Mint" },
+  { label: "OG", mintText: "0.3 SOL", buttonLabel: "Mint now!", header: "OG Mint" },
+  { label: "Public", mintText: "0.4 SOL", buttonLabel: "Mint now!", header: "Public Mint" },
   { label: "default", mintText: "Mint Paying with SOL", buttonLabel: "Mint now!", header: "Default"}
 ];
 
